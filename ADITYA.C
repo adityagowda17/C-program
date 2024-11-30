9b6b16020266ce59ed085717d303f1ce368b5a37
adityagowda17 <Aditya24852@gmail.com>
2024-11-30 02:19:27 -0800

    Add files via upload

---
+++ ADITYA.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main ()
+{
+char ch;
+printf("enter a character: ");
+scanf("%c "...,&ch);
+if(ch>='A'&&ch<='Z')
+{
+  printf("the character is uppercase:");
+}
+else
+{
+printf("the charecter is not a uppercase");
+  }
+}
